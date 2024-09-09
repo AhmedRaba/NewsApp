64f1fa6b21687963548c79a692d21d3a6791062e
ahmed <ahmedrapa91@gmail.com>
2024-09-09 21:10:25 +0300

    implement splash screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+    implementation(libs.androidx.core.splashscreen)
+
+
 }
--- app/src/main/java/com/training/newsapp/MainActivity.kt
+++ app/src/main/java/com/training/newsapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.training.newsapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.training.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
