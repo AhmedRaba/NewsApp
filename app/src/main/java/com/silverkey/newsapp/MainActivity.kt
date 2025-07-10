275ef540280ee6dcf3cae03a6997930691dfb58d
Ahmed Raba <ahmedrapa91@gmail.com>
2025-07-11 01:19:26 +0300

    Implement Splash screen and customize theme
    - Add SplashFragment.
    - Define color resources for the app's UI elements, including primary, background, surface, text, and icon colors.
    - Update MainActivity to handle visibility of BottomNavigationView and Toolbar based on the current destination.

--- app/src/main/java/com/silverkey/newsapp/MainActivity.kt
+++ app/src/main/java/com/silverkey/newsapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.silverkey.newsapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.silverkey.newsapp.databinding.ActivityMainBinding
@@ -22,15 +27,63 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+            val splashColor=ContextCompat.getColor(this, R.color.color_splash_background)
+            when (destination.id) {
+                com.silverkey.feature.R.id.splashFragment -> {
+                    navView.visibility = View.GONE
+                    binding.toolbar.visibility = View.GONE
+
+                    insetsController.isAppearanceLightStatusBars = false
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) { // Android 15+
+                            window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+                                val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+                                val navigationBarInsets = insets.getInsets(WindowInsets.Type.navigationBars())
+                                view.setBackgroundColor(splashColor)
+
+                                view.setPadding(0, statusBarInsets.top, 0, navigationBarInsets.bottom)
+                                insets
+                            }
+                        } else {
+                            window.statusBarColor = splashColor
+                            window.navigationBarColor = splashColor
+                        }
+
+
+                }
+
+                else -> {
+                    navView.visibility = View.VISIBLE
+                    binding.toolbar.visibility = View.VISIBLE
+
+                    insetsController.isAppearanceLightStatusBars = true
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) { // Android 15+
+                        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+                            val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+                            val navigationBarInsets = insets.getInsets(WindowInsets.Type.navigationBars())
+                            view.setBackgroundColor(ContextCompat.getColor(this, R.color.color_navigation_bar))
+
+                            view.setPadding(0, statusBarInsets.top, 0, navigationBarInsets.bottom)
+                            insets
+                        }
+                    } else {
+                        window.statusBarColor = ContextCompat.getColor(this, R.color.color_navigation_bar)
+                        window.navigationBarColor = ContextCompat.getColor(this, R.color.color_navigation_bar)
+                    }
+                }
+            }
+        }
     }
 }
